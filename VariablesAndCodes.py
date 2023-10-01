e420697adc3e75e0074498780a974441cdc593ab
Vishal Mittal <vishalmittalrohini@gmail.com>
2023-10-01 21:10:29 +0530

    Improved your Greetings code as well.

--- VariablesAndCodes.py
+++ VariablesAndCodes.py
@@ -1,14 +1,28 @@
 from datetime import datetime
+
+# Get the current hour
 hour = datetime.now().hour
+
+# Determine the part of the day
 if 5 <= hour <= 11:
-    partoftheday = "morning"
-
+    part_of_the_day = "morning"
 elif 12 <= hour <= 17:
-    partoftheday = "afternoon"
-
+    part_of_the_day = "afternoon"
 elif 18 <= hour <= 22:
-    partoftheday = "evening"
-
+    part_of_the_day = "evening"
 else:
-    partoftheday = "night"
-user_name = "vishal"
+    part_of_the_day = "night"
+
+# User-specific data
+user_name = "Gideon"
+
+# Greetings based on time of day
+system_Greetings = [
+    f'Hi {user_name}, good {part_of_the_day}!',
+    f'Hello {user_name}, good {part_of_the_day}!',
+    f'Good to see you {user_name}! Good {part_of_the_day}!',
+    f'Greetings, {user_name}! Good {part_of_the_day}!',
+    f'Good {part_of_the_day}, {user_name}!'
+]
+
+print(random.choice(system_Greetings))
