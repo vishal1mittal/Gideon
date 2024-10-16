9d9e2771dedd6cda3d7b25615995292af430ae4d
Vishal Mittal <71580503+vishal1mittal@users.noreply.github.com>
2024-10-16 21:16:20 +0530

    Update MydataSets.py

--- MydataSets.py
+++ MydataSets.py
@@ -1,6 +1,6 @@
 from VariablesAndCodes import *
 user_Greetings = ['hello gideon', 'hi gideon', 'good morning gideon', 'good afternoon gideon', 'good evening gideon', 'good night gideon']
-system_Greetings = ['hi '+user_name, 'hello '+user_name, 'good to see you '+user_name, 'greetings '+user_name, 'Good '+partoftheday+' '+user_name]
+system_Greetings = ['hi '+user_name, 'hello '+user_name, 'good to see you '+user_name, 'greetings '+user_name, 'Good '+partoftheday+' '+user_name, 'So long captain']
 
 user_web_search = ['can you do a web search for me', 'do a web search for me', 'do a web search', 'perform a web search', 'gideon web search', 'search something on internet for me', 'search something on google for me', 'search something on google', 'search on google']
 system_web_search = ['right away sir', 'yes sir', 'sure sir']
